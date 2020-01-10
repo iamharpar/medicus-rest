7db527a835d42a2839ed477df410a7067a5d1b25
Shubham singh <shubham1.singh15@gmail.com>
2020-01-10 14:33:26 +0530

    added model for organisation

--- medicus_rest/accounts/models.py
+++ medicus_rest/accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
+from uuid import uuid4
 from .managers import CustomUserManager
 
 
@@ -20,3 +21,15 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Organisation(models.Model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    uuid = models.UUIDField(default=uuid4, editable=False, unique=True)
+    description = models.TextField(
+        _('proper description of organisation'),
+    )
+    short_description = models.CharField(
+        _('short description of organisation (300 characters)'),
+        max_length=300
+    )
