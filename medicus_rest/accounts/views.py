4a23deefedf257e953cacc4d999a54e9c8451d5e
Shubham singh <shubham1.singh15@gmail.com>
2020-01-10 22:20:29 +0530

    set cookie after signup

--- medicus_rest/accounts/views.py
+++ medicus_rest/accounts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import status
 from .serializers import UserSerializer
 from .models import User
 
@@ -6,3 +7,10 @@ from .models import User
 class UserModelViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        if response.status_code == status.HTTP_201_CREATED:
+            auth_token = response.data['auth_token']
+            response.set_cookie('auth_token', auth_token)
+        return response
