de8039464fb562dda40d9e387bd8e2ba12033b2c
Shubham singh <shubham1.singh15@gmail.com>
2020-01-11 13:55:34 +0530

    comments and refracted code

--- medicus_rest/accounts/views.py
+++ medicus_rest/accounts/views.py
@@ -1,7 +1,7 @@
 from djoser.views import TokenCreateView, TokenDestroyView
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
-from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status
 
@@ -63,14 +63,24 @@ def check_user_logged_in(request):
     # whether the user instance have a corresponding token instance
     # associated with it. The presence of `token` instance means that
     # the user is logged in, and to logout user just delete the `user.token`
+
+    # NOTE: The djoser backend implements a default tokenAuthentication which
+    # can be found in settings, because of which this function cannot be used
+    # anyone without `auth_token` making half of this function useless.
+    # probably remove the useless part later during the development.
     auth_token = request.data.get('auth_token', None)
     if auth_token is None:
-        return JsonResponse({
+        return Response({
             'error': '`auth_token` not provided !'
-        }, status=status.HTTP_400_BAD_REQUEST)
+        }, status=status.HTTP_400_BAD_REQUEST, content_type='json')
 
     is_logged_in = Token.objects.filter(key=auth_token).exists()
 
-    return JsonResponse({
+    if is_logged_in:
+        login_status = status.HTTP_200_OK
+    else:
+        login_status = status.HTTP_401_UNAUTHORIZED
+
+    return Response({
         'logged_in': is_logged_in
-    }, status=status.HTTP_200_OK)
+    }, status=login_status, content_type='json')
